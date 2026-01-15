813d50c56f8e33523ee822c101cd91e932708ccc
theshivrajpatil <theshivrajpatil@gmail.com>
2026-01-15 11:57:46 +0530

    Added QR CODE File

---
+++ QR_Code.py
@@ -0,0 +1,18 @@
+import qrcode 
+
+def generate_qr_code(data, file_name):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4,
+    )
+    qr.add_data(data)
+    qr.make(fit = True)
+
+    img = qr.make_image(fill_color = 'black', black_color = 'White')
+    img.save(file_name)
+
+    print('qr code saved as : ', file_name)
+
+generate_qr_code('https://github.com/theshivrajpatil', '1.png')
