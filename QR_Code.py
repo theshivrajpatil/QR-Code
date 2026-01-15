1525b609fbb4e181bc4024237c6f96fbddd9dfa8
theshivrajpatil <theshivrajpatil@gmail.com>
2026-01-15 12:06:45 +0530

    added png file

--- QR_Code.py
+++ QR_Code.py
@@ -5,7 +5,7 @@ def generate_qr_code(data, file_name):
         version = 1,
         error_correction = qrcode.constants.ERROR_CORRECT_L,
         box_size = 10,
-        border = 4,
+        border = 4, 
     )
     qr.add_data(data)
     qr.make(fit = True)
